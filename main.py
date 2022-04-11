4354aecf442ee07739a30e25edff7cedfc5e080b
TimotejLang <timotej.lang@student.um.si>
2022-04-11 15:34:13 +0200

    hello, test commit new

--- main.py
+++ main.py
@@ -8,6 +8,6 @@ def main():
 
 
 # yoo bruv ehehsahs - popravek komentarja, dodan commit-msg in prepare-commit-msg hooki
-# test post commita in sporocila, yoo, new msg
+# test post commita in sporocila, yoo, new msg, novi test
 if __name__ == "__main__":
     main()
